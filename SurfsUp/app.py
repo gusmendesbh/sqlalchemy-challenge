4dd2a6e78142804ba76dd644bf497a841f359d3e
gusmendesbh <101278278@georgebrown.ca>
2023-03-05 12:34:04 -0500

    fixed issues, removed unused imports

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 import datetime as dt
-import numpy as np
+
 
 #################################################
 # Database Setup
